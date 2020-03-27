16c85da82556c3f274681c08710ec19ebb047355
TobiasHerlitz <tobiasherlitz@gmail.com>
2020-03-27 13:14:04 +0100

    Added message func and last messages to index

--- main_app/views.py
+++ main_app/views.py
@@ -9,9 +9,25 @@ from main_app.models import Message
 
 
 # Create your views here.
-class Index(LoginRequiredMixin, TemplateView):
+class Index(LoginRequiredMixin, CreateView):
 	login_url = 'Login'
 	template_name = 'index.html'
+
+	fields = ['text_content']
+	model = Message
+
+	def form_valid(self, form):
+		message_instance = form.save(commit=False)
+		message_instance.poster = User.objects.get(first_name=self.request.user)
+		message_instance.timestamp = time.strftime('%d-%b %H:%M')
+		message_instance.save()
+		return HttpResponseRedirect('/')
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(Index, self).get_context_data(**kwargs)
+		context["all_objects"] = self.model.objects.all()
+		context["last_four_objects"] = context["all_objects"][len(context["all_objects"])-4:len(context["all_objects"]):1]
+		return context
 
 class Login(LoginView):
 	template_name = 'login.html'
